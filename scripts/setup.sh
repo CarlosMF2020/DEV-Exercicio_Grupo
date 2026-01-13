6b4eca41b6f72ab5a196d0dea69e816eecb03166
fabricio <fabriciogabriel04@gmail.com>
2026-01-13 12:18:16 +0000

    alterando setup.sh para idempotente

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+
 set -e
+
+if [ -v curl ]
 sudo apt-get update
 sudo apt-get install -y curl
+
 curl -s -I http://example.com | head -n 1
+
+else 
+	echo "Não foi possível instalar o curl. Utilize -v curl" >&2
+	exit 2
+fi
+
+exit 0
